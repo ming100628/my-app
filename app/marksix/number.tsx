511f4cd19e52b5bacc4059aa58e17120ce835472
Ming Fong <ming100628@gmail.com>
2025-04-27 10:34:44 +0800

    temp refactor

--- app/marksix/number.tsx
+++ app/marksix/number.tsx
@@ -1,134 +1,74 @@
 "use client";
 import { useState } from "react";
+import { BGCOLORS, BORDERCOLORS } from "../utils/constants";
 
 export default () => {
-  const [selected, setSelected] = useState<number[]>([]);
-  const [saved, setSaved] = useState<number[][]>([]);
+  const [balls, setBalls] = useState<number[][]>([[]]);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
-  function handleBallClick(index: number, pos: boolean) {
-    if (pos) return;
-    const temp2 = [...selected];
-
-    if (temp2.includes(index)) {
-      temp2.splice(temp2.indexOf(index), 1);
+  function searchEmptyArray() {
+    if (balls.find((arr) => arr.length === 0)) {
+      return true;
     } else {
-      temp2.push(index);
-    }
-    temp2.sort((a, b) => a - b);
-    setSelected(temp2);
-    if (temp2.length >= 6) {
-      const temp3 = [...saved];
-
-      temp3.push(temp2);
-      setSaved(temp3);
-      setSelected([]);
+      return false; // Return -1 if no empty array is found
     }
   }
 
-  function renderColor(index: number, type: string, position: boolean) {
-    const bgcolors = [
-      "bg-red-500",
-      "bg-blue-500",
-      "bg-pink-500",
-      "bg-yellow-900",
-      "bg-green-500",
-      "bg-orange-500",
-      "bg-gray-500",
-      "bg-purple-500",
-    ];
-    const bordercolors = [
-      "border-red-500",
-      "border-blue-500",
-      "border-pink-500",
-      "border-yellow-900",
-      "border-green-500",
-      "border-orange-500",
-      "border-gray-500",
-      "border-purple-500",
-    ];
-    if (position) {
-      if (type == "text") return "text-black";
-      if (type == "border") return bordercolors[index % 8];
-      if (type == "bg") return "bg-white";
-    }
+  function handleBallClick(index: number) {
+    const temp2 = [...balls];
 
+    if (temp2[selectedIndex].includes(index)) {
+      temp2[selectedIndex].splice(temp2[selectedIndex].indexOf(index), 1);
+    } else if (temp2[selectedIndex].length < 6) {
+      temp2[selectedIndex].push(index);
+    }
+    temp2[selectedIndex].sort((a, b) => a - b);
+    setBalls(temp2);
+    if (balls[selectedIndex].length >= 6) {
+      const temp3 = [...balls];
+
+      if (!searchEmptyArray()) temp3.push([]);
+      setSelectedIndex(temp3.length - 1);
+      setBalls(temp3);
+    }
+  }
+
+  function renderColor(index: number, type: string) {
     if (type === "text") {
-      if (selected.includes(index)) return "text-white";
-      return "text-black";
+      return "text-white";
     }
+
     if (type === "border") {
-      return bordercolors[index % 8];
-    } else {
-      if (selected.includes(index)) return bgcolors[index % 8];
-      return "bg-white";
+      return BORDERCOLORS[index % 8];
     }
+
+    return BGCOLORS[index % 8];
   }
 
-  function renderBall(index: number, pos: boolean, pos2: number = -1) {
-    if (pos2 !== -1) {
-      return (
-        <button
-          key={index}
-          className={`rounded-full ${renderColor(
-            index,
-            "border",
-            pos
-          )} border-4 ${renderColor(index, "bg", pos)} ${renderColor(
-            index,
-            "text",
-            pos
-          )} w-16 h-16 flex items-center justify-center font-bold text-2xl`}
-          onClick={() => {
-            setSelected(selected.filter((_, i) => i !== pos2));
-          }}
-        >
-          {index}
-        </button>
-      );
-    }
+  function renderBall(index: number, left: boolean) {
     return (
       <button
         key={index}
-        className={`rounded-full ${renderColor(
-          index,
-          "border",
-          pos
-        )} border-4 ${renderColor(index, "bg", pos)} ${renderColor(
-          index,
-          "text",
-          pos
-        )} w-16 h-16 flex items-center justify-center font-bold text-2xl`}
-        onClick={() => handleBallClick(index, pos)}
+        className={`rounded-full ${
+          !left && renderColor(index, "border")
+        } border-4 ${!left && renderColor(index, "bg")} ${
+          !left && renderColor(index, "text")
+        } w-16 h-16 flex items-center justify-center font-bold text-2xl`}
+        onClick={left ? () => {} : () => handleBallClick(index)}
       >
         {index}
       </button>
     );
   }
 
-  function renderSelected() {
-    if (selected.length > 0) {
-      return (
-        <div
-          key={saved.length}
-          className={`flex space-x-4 items-center justify-center rounded-md border-8 ${renderColor(
-            saved.length,
-            "border",
-            false
-          )}`}
-        >
-          {selected.map((number, index) => renderBall(number, true, index))}
-        </div>
-      );
-    } else return <div></div>;
-  }
   return (
     <div className="flex h-full w-full bg-grey-100">
       <div className="w-[35%] h-full bg-red-300 border-8 rounded-md border-gray-500 flex-col-2 items-center justify-center ">
         <div className="w-full h-full items-center justify-center flex flex-col">
           {Array.from({ length: 7 }).map((_, i) => (
             <div className="flex space-x-3 space-y-3" key={i}>
-              {Array.from({ length: 7 }).map(
-                (_, j) => renderBall(i * 7 + j + 1, false) // Adjust the index to start from 1
+              {Array.from({ length: 7 }).map((_, j) =>
+                renderBall(i * 7 + j + 1, false)
               )}
             </div>
           ))}
@@ -136,30 +76,25 @@ export default () => {
       </div>
       <div className="w-[65%] h-full bg-blue-300 border-8 rounded-md border-purple-500 flex-col-2 items-center justify-center">
         <div className="w-[100%] h-[10%] bg-gray-400 flex items-center justify-center text-xl font-bold">
-          Saved Selections
+          balls Selections
         </div>
         <div className="w-full h-[90%] items-center justify-center text-2xl font-bold ">
-          {saved.map((selection, index) => (
+          {balls.map((selection, index) => (
             <div
               key={index}
-              className={`flex space-x-4 items-center justify-center rounded-md border-8 ${renderColor(
-                index,
-                "border",
-                false
-              )}`}
+              className={`flex space-x-4 items-center justify-center rounded-md border-8`}
             >
               <div className="h-full w-[80%] space-x-4 flex items-center justify-center text-2xl font-bold">
-                {selection.map((number) => renderBall(number, true))}
+                {selection.map((number) => renderBall(number, false))}
               </div>
               <button
                 className="h-full w-[20%]"
-                onClick={() => setSaved(saved.filter((_, i) => i !== index))}
+                onClick={() => setBalls(balls.filter((_, i) => i !== index))}
               >
                 Delete
               </button>
             </div>
           ))}
-          {renderSelected()}
         </div>
       </div>
     </div>
